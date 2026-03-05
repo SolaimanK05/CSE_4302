d12f8e4ccdc1ea1d68c2b1217cf24db102c1e4ce
Pluto <165196399+SolaimanK05@users.noreply.github.com>
2026-03-06 01:59:59 +0600

    Restarting fresh

--- ConsoleUI.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSOLE_UI_H
-#define CONSOLE_UI_H
-
-class ConsoleUI {
-public:
-    void start();
-};
-
-#endif
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "ConsoleUI.h"
-
-int main() {
-    ConsoleUI ui;
-    ui.start();
-    return 0;
-}
